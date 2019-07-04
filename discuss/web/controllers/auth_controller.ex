b95d40f22f1dac4b684d3dea80defbf910c602d0
Eric Schmucker <eschmucker@unstoppablesoftware.com>
2019-07-04 11:32:05 -0600

    Add user table and store github authorized user info in it

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -1,12 +1,48 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
+  alias Discuss.User
 
-  def callback(conn, params) do
-    IO.inspect "0000000000000"
-    IO.inspect conn.assigns
-    IO.inspect "1111111111111"
-    IO.inspect params
-    IO.inspect "2222222222222"
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{
+      email: auth.info.email,
+      name: auth.info.name,
+      provider: Atom.to_string(auth.provider),
+      token: auth.credentials.token
+    }
+    changeset = User.changeset(%User{}, user_params)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        error_msg = format_changeset_errors(changeset)
+        conn
+        |> put_flash(:error, "Error signing in: #{error_msg}")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
+  end
+
+  defp format_changeset_errors(changeset) do
+    errors_map = Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+    (for {key, values} <- errors_map, value <- values, do: "'#{key}' #{value}")
+    |> Enum.join(", ")
   end
 end
